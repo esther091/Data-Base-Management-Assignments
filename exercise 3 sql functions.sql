9344c433ce8445fc0e7a8e0f9759777abbac0416
esthermgbedo <esthermgbedo@gmail.com>
2025-10-18 11:40:02 +0100

    Add files via upload

--- exercise 3 sql functions.sql
+++ exercise 3 sql functions.sql
@@ -40,6 +40,7 @@ select price, price-(0.15*price) "sale price after discount" from products
 SELECT name, TO_CHAR(release_date, 'Day') AS day_of_week
 FROM products;
 
+
 -- Write a query to find all products released in the current month, regardless of year.
 select name, release_date,  extract(month from release_date) from products
 order by extract(month from release_date) desc
@@ -72,7 +73,6 @@ SELECT
     MAX(price) AS maximum_price,
     COUNT(*) AS total_products
 FROM products;
-
 
 
 
